92265fd1b64a1ffdb8d00838a331358132b648c5
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-23 15:16:59 +0100

    first commit

---
+++ src/test/java/com/example/livraigo/LivraigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livraigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivraigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
