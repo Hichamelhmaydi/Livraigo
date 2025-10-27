afaa885763aced458e93829ed70b352b6078ed08
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-27 12:10:09 +0100

    Removed TourComparisonDTO and added fields to OptimizedTourResponseDTO

--- src/main/java/com/livraigo/dto/OptimizedTourResponseDTO.java
+++ src/main/java/com/livraigo/dto/OptimizedTourResponseDTO.java
@@ -1,5 +1,20 @@
 package com.livraigo.dto;
 
-public class OptimizedTourResponseDTO {
+import com.livraigo.model.enums.OptimizationAlgorithm;
+import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class OptimizedTourResponseDTO {
+    
+    @NotNull(message = "L'algorithme est obligatoire")
+    private OptimizationAlgorithm algorithm;
+    
+    @NotNull(message = "L'ID du tour est obligatoire")
+    private Long tourId;
+    
+    private List<Long> deliveryIds = new ArrayList<>();
 }
