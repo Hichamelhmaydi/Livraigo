a628f241d0341cafbb5014a80d4d3dca68bc3257
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-27 10:49:48 +0100

    Refactor Warehouse entity class: add annotations, fields and constructors

--- src/main/java/com/livraigo/model/entity/Warehouse.java
+++ src/main/java/com/livraigo/model/entity/Warehouse.java
@@ -1,5 +1,40 @@
 package com.livraigo.model.entity;
 
-public class Warehouse {
+import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "warehouses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Warehouse {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull(message = "Le nom est obligatoire")
+    @Column(unique = true, nullable = false)
+    private String name;
+    
+    @NotNull(message = "L'adresse est obligatoire")
+    @Column(nullable = false)
+    private String address;
+    
+    @Column(nullable = false)
+    private Double latitude;
+    
+    @Column(nullable = false)
+    private Double longitude;
+    
+    @Column(nullable = false)
+    private LocalTime openingTime;
+    
+    @Column(nullable = false)
+    private LocalTime closingTime;
 }
