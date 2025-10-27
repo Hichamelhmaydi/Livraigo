24f44285616a9bd19138d8ae0d228d5e935063a0
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-27 10:21:40 +0100

    Refactor Tour entity: add imports, annotations, fields and relationships

--- src/main/java/com/livraigo/model/entity/Tour.java
+++ src/main/java/com/livraigo/model/entity/Tour.java
@@ -1,5 +1,46 @@
 package com.livraigo.model.entity;
 
-public class Tour {
+import com.livraigo.model.enums.OptimizationAlgorithm;
+import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tours")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tour {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull(message = "La date est obligatoire")
+    @Column(nullable = false)
+    private LocalDate date;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OptimizationAlgorithm algorithm;
+    
+    private Double totalDistance;
+    
+    @OneToMany(mappedBy = "tour", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("order ASC")
+    @Builder.Default
+    private List<Delivery> deliveries = new ArrayList<>();
 }
