92c4a98ade49f87e4443d4da36fa806d286ddf76
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-27 10:49:10 +0100

    Refactored Vehicle entity class with added annotations and fields

--- src/main/java/com/livraigo/model/entity/Vehicle.java
+++ src/main/java/com/livraigo/model/entity/Vehicle.java
@@ -1,5 +1,42 @@
 package com.livraigo.model.entity;
 
-public class Vehicle {
+import com.livraigo.model.enums.VehicleType;
+import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "vehicles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull(message = "La plaque d'immatriculation est obligatoire")
+    @Column(unique = true, nullable = false)
+    private String licensePlate;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleType type;
+    
+    @DecimalMin(value = "0.1", message = "La capacité de poids doit être supérieure à 0")
+    @Column(nullable = false)
+    private Double maxWeight;
+    
+    @DecimalMin(value = "0.1", message = "La capacité de volume doit être supérieure à 0")
+    @Column(nullable = false)
+    private Double maxVolume;
+    
+    @Column(nullable = false)
+    private Integer maxDeliveries;
+    
+    private Boolean available;
 }
