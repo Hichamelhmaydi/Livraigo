68a70535be84db84408a030909a4dec6e604536e
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-28 14:55:56 +0100

    Added interface definitions for DeliveryService, TourService and WarehouseService

--- src/main/java/com/livraigo/service/interfaces/DeliveryService.java
+++ src/main/java/com/livraigo/service/interfaces/DeliveryService.java
@@ -1,5 +1,17 @@
 package com.livraigo.service.interfaces;
 
-public class DeliveryService {
-    
+import com.livraigo.dto.DeliveryRequestDTO;
+import com.livraigo.model.entity.Delivery;
+import com.livraigo.model.entity.enums.DeliveryStatus;
+
+import java.util.List;
+
+public interface DeliveryService {
+    List<Delivery> findAll();
+    Delivery findById(Long id);
+    Delivery save(DeliveryRequestDTO deliveryDTO);
+    Delivery update(Long id, DeliveryRequestDTO deliveryDTO);
+    void deleteById(Long id);
+    List<Delivery> findByStatus(DeliveryStatus status);
+    List<Delivery> findUnassignedDeliveries();
 }
--- src/main/java/com/livraigo/service/interfaces/WarehouseService.java
+++ src/main/java/com/livraigo/service/interfaces/WarehouseService.java
@@ -1,5 +1,13 @@
 package com.livraigo.service.interfaces;
 
-public class WarehouseService {
-    
+import com.livraigo.model.entity.Warehouse;
+
+import java.util.List;
+
+public interface WarehouseService {
+    List<Warehouse> findAll();
+    Warehouse findById(Long id);
+    Warehouse save(Warehouse warehouse);
+    Warehouse update(Long id, Warehouse warehouse);
+    void deleteById(Long id);
 }
