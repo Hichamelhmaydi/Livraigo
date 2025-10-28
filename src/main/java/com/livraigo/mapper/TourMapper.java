850c9708bd45eff2130a7e617925566c16611c09
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-28 09:39:21 +0100

    Added ModelMapper instances to mapper classes and implemented toEntity and toDto methods for Delivery, Tour, Vehicle and Warehouse mappers.

--- src/main/java/com/livraigo/mapper/TourMapper.java
+++ src/main/java/com/livraigo/mapper/TourMapper.java
@@ -1,5 +1,22 @@
 package com.livraigo.mapper;
 
-public class TourMapper {
+import com.livraigo.dto.TourRequestDTO;
+import com.livraigo.model.entity.Tour;
+import org.modelmapper.ModelMapper;
 
+public class TourMapper {
+    
+    private final ModelMapper modelMapper;
+
+    public TourMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+    
+    public Tour toEntity(TourRequestDTO dto) {
+        return modelMapper.map(dto, Tour.class);
+    }
+    
+    public TourRequestDTO toDto(Tour entity) {
+        return modelMapper.map(entity, TourRequestDTO.class);
+    }
 }
--- src/main/java/com/livraigo/mapper/WarehouseMapper.java
+++ src/main/java/com/livraigo/mapper/WarehouseMapper.java
@@ -1,5 +1,17 @@
 package com.livraigo.mapper;
 
-public class WarehouseMapper {
+import com.livraigo.model.entity.Warehouse;
+import org.modelmapper.ModelMapper;
 
+public class WarehouseMapper {
+    
+    private final ModelMapper modelMapper;
+
+    public WarehouseMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+    
+    public Warehouse toEntity(Warehouse entity) {
+        return modelMapper.map(entity, Warehouse.class);
+    }
 }
