ef40d74f8c9e04351d3ccf50ff6d0a9489bf00c1
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-28 16:03:48 +0100

    Refactor DateTimeUtil and TourValidator classes, remove unused classes (DateTimeUtil, DistanceCalculator, TourValidator) from com.livraigo.util package.

--- src/main/java/com/livraigo/service/util/DateTimeUtil.java
+++ src/main/java/com/livraigo/service/util/DateTimeUtil.java
@@ -1,5 +1,21 @@
 package com.livraigo.service.util;
 
-public class DateTimeUtil {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
+public class DateTimeUtil {
+    
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+    
+    public LocalTime parseTime(String timeString) {
+        return LocalTime.parse(timeString, TIME_FORMATTER);
+    }
+    
+    public String formatTime(LocalTime time) {
+        return time.format(TIME_FORMATTER);
+    }
+    
+    public boolean isWithinTimeWindow(LocalTime time, LocalTime start, LocalTime end) {
+        return !time.isBefore(start) && !time.isAfter(end);
+    }
 }
--- src/main/java/com/livraigo/util/DateTimeUtil.java
+++
@@ -1,5 +0,0 @@
-package com.livraigo.util;
-
-public class DateTimeUtil {
-
-}
